6789116b32f9b58685a941a159d074786cf82a46
Aryan D <aryan.d.dalal@gmail.com>
2023-06-11 23:44:52 +0530

    Changes
    Added the main features

--- Player.cs
+++ Player.cs
@@ -5,6 +5,20 @@ public class Player : MonoBehaviour
     Inventory inventory;
     public Transform head;
     public PlayerItemInteractor itemInteractor;
+    public PlayerMovement movement;
+    public MovementConfig movementConfig;
+    private void Start()
+    {
+        if (!TryGetComponent(out movement))
+        {
+            Debug.LogWarning("No movement Script attached!");
+        }
+        else
+        {
+            movement.Initiate(movementConfig);
+        }
+    }
+
     void Update()
     {
         var res = itemInteractor.CheckForItem(head);
@@ -13,5 +27,13 @@ public class Player : MonoBehaviour
             var itemObject = res.Value;
             itemObject.PickMeUp(inventory);
         }
+        
+        if(movement != null)
+        {
+            Vector2 axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Vector2 mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            movement.Move(axis,mouse);
+        }
+
     }
 }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,18 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public CharacterController cc;
+    [SerializeField]
+    public GameObject Head;
+    [Header("Acceleration due to gravity")]
+    public float Gravity;
+    [SerializeField]
+    public Transform GroundCheck;
+    [SerializeField]
+    public LayerMask GroundLayerMask;
+    public bool IsGrounded ;//{ get;private set; }
+    public const float GroundCheckRadius = 0.5f;
+    public float MovementSpeed;
+    public float Sensitivity;
+    private float _m_jumpheight_;
+    private float xrot = 0;
+    public float JumpHeight
     {
-        
+        get
+        {
+            return _m_jumpheight_;
+        }
+        set
+        {
+            if(value >= 0)
+            {
+                _m_jumpheight_ = value;
+            }
+            else
+            {
+                _m_jumpheight_=0;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if(!TryGetComponent<CharacterController>(out cc))
+        {
+            Debug.LogWarning("No charecter controller found. Destroying this script.");
+            Destroy(this); 
+            return;
+        }
+        if(Head == null)
+        {
+            Debug.LogWarning($"{nameof(Head)} is null!!!");
+        }
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(GroundCheck.position, GroundCheckRadius);
+    }
+    public void Initiate(MovementConfig config)
+    {
+        Gravity = config.Gravity;
+        MovementSpeed = config.MovementSpeed;
+        Sensitivity = config.Sensitivity;
+    }
+    Vector3 move = Vector3.zero;
+    /// <summary>
+    /// Moves and rotates the player using the axises
+    /// by default, space is used as the jump key.
+    /// </summary>
+    /// <param name="axis"></param>
+    /// <param name="mouse"></param>
+    public void Move(Vector2 axis, Vector2 mouse, KeyCode jump = KeyCode.Space)
+    {
+        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundCheckRadius, GroundLayerMask);
+
+        float x = axis.x * MovementSpeed * Time.deltaTime;
+        float z = axis.y * MovementSpeed * Time.deltaTime;
+
+        float mx = mouse.x * Sensitivity * Time.deltaTime;
+        float my = mouse.y * Sensitivity * Time.deltaTime;
+
+        xrot -= my;
+        xrot = Mathf.Clamp(xrot, -90, 90);
+        Head.transform.rotation = Quaternion.Euler(xrot, 0, 0);
+        transform.Rotate(Vector3.up * mx);
+
+        move = transform.forward * z + transform.right * z;
+
+        if (Input.GetKeyDown(jump) && IsGrounded)
+        {
+            move.y = Mathf.Sqrt(Mathf.Abs(2*Gravity*JumpHeight)) * Time.deltaTime;
+        }
+        else if (IsGrounded)
+        {
+            move.y = 0;
+        }
+        else 
+        {
+            move.y += Gravity * Time.deltaTime;
+        }
+    }
+}
+
+public class MovementConfig
+{
+    public float MovementSpeed = 10;
+    public float Sensitivity = 5;
+    public float Gravity = -9.8f;
+    public float JumpHeight = 1;
 }
